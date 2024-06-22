b72653640c069c18943b7e1f4e897acfefa6ce24
Mitali Gupta <mitaligupta726@gmail.com>
2024-06-22 15:29:30 -0400

    passing vaues with directives ngForm

--- mean-singlepage/src/app/post-create.component.ts
+++ mean-singlepage/src/app/post-create.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { Post } from './post.model';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-post-create',
@@ -10,10 +11,13 @@ export class PostCreateComponent {
   enteredContent= "";
   @Output() postCreated = new EventEmitter();
 
-  onAddPost() {
+  onAddPost(form: NgForm) {
+    if(form.invalid){
+      return;
+    }
     const post: Post = {
-      title: this.enteredTitle, 
-      content: this.enteredContent
+      title: form.value.title, 
+      content: form.value.content
     };
     this.postCreated.emit(post);
   }
