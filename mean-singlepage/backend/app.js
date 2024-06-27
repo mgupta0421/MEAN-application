419faf9299f06f155352a6ed4a49e50c13beb5eb
Mitali Gupta <mitaligupta726@gmail.com>
2024-06-27 12:00:36 -0400

    setting environment file

--- mean-singlepage/backend/app.js
+++ mean-singlepage/backend/app.js
@@ -3,14 +3,19 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const postRoutes = require('./routes/posts');
 
+require('dotenv').config();
+
+const dbPassword = process.env.MONGO_PASSWORD;
+const dbUser = process.env.MONGO_USER;
+
 const app = express();
 
-mongoose.connect("mongodb+srv://mitaligupta726:<password>@cluster0.vlwuaqq.mongodb.net/node-anguler?retryWrites=true&w=majority&appName=Cluster0")
+mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.vlwuaqq.mongodb.net/node-anguler?retryWrites=true&w=majority&appName=Cluster0`)
     .then(() => {
         console.log("Connected to database");
     })
-    .catch(() => {
-        console.log("Connection not working");
+    .catch((error) => {
+        console.log("Connection not working" +error);
     });
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
